39b2a2200d0de3c7f26ab5cd908b8e230ac0475e
bogdan-jocic <bogdanjsd@gmail.com>
2025-05-11 16:54:00 +0200

    added fullscreen

---
+++ autoload/global.gd
@@ -0,0 +1,10 @@
+extends Node
+
+
+func _input(event):
+	if event.is_action_pressed("fullscreen"):
+		if get_window().mode == Window.MODE_EXCLUSIVE_FULLSCREEN:
+			get_window().mode = Window.MODE_WINDOWED
+		else:
+			get_window().mode = Window.MODE_EXCLUSIVE_FULLSCREEN
+			
